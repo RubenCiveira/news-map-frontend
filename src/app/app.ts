255f8e5d06291857149384da7514812a32494a70
Ruben civeira <rubenciveira@gmail.com>
2026-01-21 13:26:20 +0100

    feat: split on lazy components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './core/ui/toolbar/toolbar';
-import { MapComponent } from './features/news-map/map/map';
 import { AppwriteAuthService } from './core/appwrite/appwrite-auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToolbarComponent, MapComponent],
+  imports: [RouterOutlet, ToolbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
